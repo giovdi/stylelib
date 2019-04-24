4c532c833fd0640c5232a927aefe45c4b1961a92
Francesco Saverio Giordano <fs.giordano@gmail.com>
2019-04-24 23:46:23 +0200

    aggiunta classe BoxStat al tema Minimal Art

--- src/Minimalart/BoxStat.php
+++ src/Minimalart/BoxStat.php
@@ -20,122 +20,75 @@ class BoxStat extends \DeployStudio\Style\Base\BoxStat {
 	 * @return void
 	 */
 	static function stat ($variante, $bg, $icona, $titolo, $valore, $ext = null, $boxclass = 'col-12',
-		$link = null, $testo_link = 'Maggiori dettagli') {
+		$link = null, $testo_link = 'More info', $progress_percent = null, $progress_description = null) {
 		switch ($variante) {
 			case 1:
-				$content = '<!-- START card-->
-				<div class="card bg-'.$bg.' border-0">
-					<div class="card-header">
-						<div class="row align-items-center">
-							<div class="col-3">
-								<em class="'.$icona.' fa-5x"></em>
-							</div>
-							<div class="col-9 text-right">
-								<div class="text-lg">'.$valore.'
-									'.(!is_null($ext) ? '<small>'.$ext.'</small>' : '').'
-								</div>
-								<p class="m-0">'.$titolo.'</p>
-							</div>
-						</div>
-					</div>'
-					.(!is_null($link) ? '
-					<a class="card-footer bg-gray-dark bt0 clearfix btn-block d-flex" href="'.$link.'">
-						<span>'.$testo_link.'</span>
-						<span class="ml-auto">
-							<em class="fa fa-chevron-circle-right"></em>
+				$content = '
+				<div class="info-box">
+					<span class="info-box-icon bg-'.$bg.'"><i class="'.$icona.'"></i></span>
+	
+					<div class="info-box-content">
+						<span class="info-box-number">'.$valore.'
+							'.(!is_null($ext) ? '<small>'.$ext.'</small>' : '').'
 						</span>
-					</a>' : '')
-					.'
+						<span class="info-box-text">'.$titolo.'</span>
+					</div>
+					<!-- /.info-box-content -->
 				</div>
-				<!-- END card-->';
+				<!-- /.info-box -->';
 				break;
 
 			case 2:
-				$content = '<!-- START card-->
-				<div class="card flex-row align-items-center align-items-stretch border-0">
-					<div class="col-4 d-flex align-items-center bg-'.$bg.'-dark justify-content-center rounded-left">
-						<em class="'.$icona.' fa-3x"></em>
-					</div>
-					<div class="col-8 py-3 bg-'.$bg.' rounded-right">
-						<div class="h2 mt-0">'.$valore.'
+				$content = '
+				<div class="info-box bg-'.$bg.'">
+					<span class="info-box-icon push-bottom"><i class="'.$icona.'"></i></span>
+		
+					<div class="info-box-content">
+						<span class="info-box-text">'.$titolo.'</span>
+						<span class="info-box-number">'.$valore.'
 							'.(!is_null($ext) ? '<small>'.$ext.'</small>' : '').'
-						</div>
-						<div class="text-uppercase">'.$titolo.'</div>
-					</div>
-				</div>
-				<!-- END card-->';
-				break;
+						</span>
 
+						'.(!is_null($progress_percent) ? '
+						<div class="progress">
+							<div class="progress-bar bg-white" style="width: '.$progress_percent.'%;"></div>
+						</div>
+						' : '')
+						.'
+
+						'.(!is_null($progress_description) ? '
+						<span class="progress-description">
+							'.$progress_description.'
+						</span>
+						' : '')
+						.'
+					</div>
+					<!-- /.info-box-content -->
+				</div>';
+				break;
+				
 			case 3:
-				$content = '<!-- START card-->
-				<div class="card flex-row align-items-center align-items-stretch border-0">
-					<div class="col-4 d-flex align-items-center bg-'.$bg.' justify-content-center rounded-left">
-						<em class="'.$icona.' fa-3x"></em>
-					</div>
-					<div class="col-8 py-3 text-'.$bg.' rounded-right">
-						<div class="h2 mt-0">'.$valore.'
+				$content = '
+				<div class="small-box bg-'.$bg.'">
+					<div class="inner">
+						<h3>'.$valore.'
 							'.(!is_null($ext) ? '<small>'.$ext.'</small>' : '').'
-						</div>
-						<div class="text-uppercase">'.$titolo.'</div>
+						</h3>
+						<p>'.$titolo.'</p>
 					</div>
-				</div>
-				<!-- END card-->';
-				break;
+					<div class="icon">
+						<i class="'.$icona.'"></i>
+					</div>
 
-			case 4:
-				$content = '<!-- START card-->
-				<div class="card bg-'.$bg.'-dark border-0">
-					<div class="row align-items-center mx-0">
-						<div class="col-4 text-center">
-							<em class="'.$icona.' fa-3x"></em>
-						</div>
-						<div class="col-8 py-4 bg-'.$bg.' rounded-right">
-							<div class="h1 m-0 text-bold">'.$valore.'
-								'.(!is_null($ext) ? '<small>'.$ext.'</small>' : '').'
-							</div>
-							<div class="text-uppercase">'.$titolo.'</div>
-						</div>
-					</div>'
+					'
 					.(!is_null($link) ? '
-					<a class="card-footer bg-gray-dark bt0 clearfix btn-block d-flex" href="'.$link.'">
-						<span>'.$testo_link.'</span>
-						<span class="ml-auto">
-							<em class="fa fa-chevron-circle-right"></em>
-						</span>
+					<a href="'.$link.'" class="small-box-footer">
+						'.$testo_link.' <i class="fa fa-arrow-right"></i>
 					</a>' : '')
 					.'
-				</div>
-				<!-- END card-->';
+				</div>';
 				break;
-
-			case 5:
-				$content = '<!-- START card-->
-				<div class="card border-0">
-					<div class="row row-flush">
-						<div class="col-4 bg-'.$bg.' text-center d-flex align-items-center justify-content-center rounded-left">
-							<em class="'.$icona.' fa-2x"></em>
-						</div>
-						<div class="col-8">
-							<div class="card-body text-center">
-								<h4 class="mt-0">'.$valore.'
-									'.(!is_null($ext) ? '<small>'.$ext.'</small>' : '').'
-								</h4>
-								<p class="mb-0 text-muted">'.$titolo.'</p>
-							</div>
-						</div>
-					</div>'
-					.(!is_null($link) ? '
-					<a class="card-footer bg-gray-dark bt0 clearfix btn-block d-flex" href="'.$link.'">
-						<span>'.$testo_link.'</span>
-						<span class="ml-auto">
-							<em class="fa fa-chevron-circle-right"></em>
-						</span>
-					</a>' : '')
-					.'
-				</div>
-				<!-- END card-->';
-				break;
-
+				
 			default:
 				//do nothing
 		}
